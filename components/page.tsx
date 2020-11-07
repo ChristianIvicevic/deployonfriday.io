dbd4948476c70e781c96941911cb638d97f829c1
Christian Ivicevic <christian.ivicevic@comsysto.com>
2020-11-07 13:57:45 +0100

    feat: add favicon and manifest

--- components/page.tsx
+++ components/page.tsx
@@ -29,6 +29,24 @@ export const Page: FC<Props> = ({
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
         <meta charSet="utf-8" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
         <title>
           {title === undefined
             ? `${SiteMetadata.title} \u2014 A blog by ${SiteMetadata.author.name}`
