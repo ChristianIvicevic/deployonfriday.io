4ad2510e8c6a1544e8db54e9b7cc3537e08b6a68
Christian Ivicevic <christian.ivicevic@comsysto.com>
2020-11-09 01:56:55 +0100

    feat: improve seo urls and canonical urls

--- components/seo.tsx
+++ components/seo.tsx
@@ -4,39 +4,40 @@ import Head from 'next/head';
 type Props = {
   description?: string;
   title?: string;
-  url?: string;
 };
 
 export const Seo = ({
   description = `Personal blog by ${SiteMetadata.author.name}. ${SiteMetadata.author.summary}`,
   title = SiteMetadata.title,
-  url = '',
-}: Props) => (
-  <Head>
-    <meta name="description" content={description} key="description" />
-    <meta property="og:title" content={title} key="ogtitle" />
-    <meta
-      property="og:url"
-      content={`${SiteMetadata.siteUrl}/${url}`}
-      key="ogurl"
-    />
-    <meta
-      property="og:site_name"
-      content={SiteMetadata.title}
-      key="ogsitename"
-    />
-    <meta property="og:description" content={description} key="ogdesc" />
-    <meta property="twitter:card" content="summary" key="twittercard" />
-    <meta
-      property="twitter:creator"
-      content={SiteMetadata.author.name}
-      key="twittercreator"
-    />
-    <meta property="twitter:title" content={title} key="twittertitle" />
-    <meta
-      property="twitter:description"
-      content={description}
-      key="twitterdesc"
-    />
-  </Head>
-);
+}: Props) => {
+  const url =
+    SiteMetadata.siteUrl +
+    (typeof window !== 'undefined' ? window.location.pathname : '');
+
+  return (
+    <Head>
+      <meta name="description" content={description} key="description" />
+      <meta property="og:title" content={title} key="ogtitle" />
+      <meta property="og:url" content={url} key="ogurl" />
+      <meta
+        property="og:site_name"
+        content={SiteMetadata.title}
+        key="ogsitename"
+      />
+      <meta property="og:description" content={description} key="ogdesc" />
+      <meta property="twitter:card" content="summary" key="twittercard" />
+      <meta
+        property="twitter:creator"
+        content={SiteMetadata.author.name}
+        key="twittercreator"
+      />
+      <meta property="twitter:title" content={title} key="twittertitle" />
+      <meta
+        property="twitter:description"
+        content={description}
+        key="twitterdesc"
+      />
+      <link rel="canonical" href={url} key="canonical" />
+    </Head>
+  );
+};
