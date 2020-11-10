15e0f7837c1f9ddc004fa1cd6096705c3ac35b6c
Christian Ivicevic <christian.ivicevic@comsysto.com>
2020-11-10 21:22:34 +0100

    perf: apply suggested lighthouse changes

--- components/profile.tsx
+++ components/profile.tsx
@@ -3,9 +3,11 @@ import { SiteMetadata } from 'constants/site-metadata';
 export const Profile = () => (
   <div className="profile">
     <img
-      src="/images/profile.png"
+      src="/images/profile.jpg"
       alt={SiteMetadata.author.name}
       className="profile__image"
+      width={48}
+      height={48}
     />
     <p className="profile__description">
       Personal blog by{' '}
