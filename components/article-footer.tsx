5075303fd28c79d18f87f398e86abe768d4c5d0b
Christian Ivicevic <christian.ivicevic@comsysto.com>
2020-11-13 02:35:48 +0100

    feat: add feedback link to article footer

--- components/article-footer.tsx
+++ components/article-footer.tsx
@@ -1,5 +1,6 @@
 import { Icon } from '@blueprintjs/core';
 import classNames from 'classnames';
+import { SiteMetadata } from 'constants/site-metadata';
 import Link from 'next/link';
 
 export type PostFooterLink = {
@@ -16,39 +17,51 @@ export const ArticleFooter = ({
   previousLink,
   nextLink,
 }: ArticleFooterProps) => (
-  <nav className="article__footer">
-    {previousLink ? (
-      <div className="article__nav-link">
-        <Link href={previousLink.slug}>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="article__nav-anchor">
-            <Icon icon="arrow-left" className="article__nav-icon" />
-            {previousLink.title}
-          </a>
-        </Link>
-      </div>
-    ) : (
-      <>
-        {/* We need this empty div in case we're at the first post. */}
-        <div />
-      </>
-    )}
-    {nextLink && (
-      <div className="article__nav-link">
-        <Link href={nextLink.slug}>
-          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a className="article__nav-anchor">
-            {nextLink.title}
-            <Icon
-              icon="arrow-right"
-              className={classNames(
-                'article__nav-icon',
-                'article__nav-icon--next',
-              )}
-            />
-          </a>
-        </Link>
-      </div>
-    )}
-  </nav>
+  <>
+    <nav className="article__footer">
+      {previousLink ? (
+        <div className="article__nav-link">
+          <Link href={previousLink.slug}>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className="article__nav-anchor">
+              <Icon icon="arrow-left" className="article__nav-icon" />
+              {previousLink.title}
+            </a>
+          </Link>
+        </div>
+      ) : (
+        <>
+          {/* We need this empty div in case we're at the first post. */}
+          <div />
+        </>
+      )}
+      {nextLink && (
+        <div className="article__nav-link">
+          <Link href={nextLink.slug}>
+            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+            <a className="article__nav-anchor">
+              {nextLink.title}
+              <Icon
+                icon="arrow-right"
+                className={classNames(
+                  'article__nav-icon',
+                  'article__nav-icon--next',
+                )}
+              />
+            </a>
+          </Link>
+        </div>
+      )}
+    </nav>
+    <hr />
+    <div className="article__feedback">
+      <a
+        href={SiteMetadata.feedbackLink}
+        target="_blank"
+        rel="nofollow noopener noreferrer"
+      >
+        Leave a comment or share feedback on Github
+      </a>
+    </div>
+  </>
 );
